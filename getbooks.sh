b9eb49df640b1e2b62f7792f9cf6d4308a5b3a1e
Benjamin Prieto <bjprieto@ucdavis.edu>
2023-05-30 21:42:20 -0700

    initialized getbooks

---
+++ getbooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash/
+
+# Input Quality Control
+
+# one file --> chk one argument
+# file contains a list of web addresses, plain text UTF-8 format --> chk format
+# address on Gutenberg.org --> check can wget
+
+
+# Effects
+
+# scrub for author, title
+# chk book exists in repo
+# if not, download in a directory named after author, file name with book title
