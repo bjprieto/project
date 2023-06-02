efa35eca71663f2de12528d6e5fbb4a173ea785d
Benjamin Prieto <bjprieto@ucdavis.edu>
2023-06-01 17:38:07 -0700

    added references and started quality control scripting

--- getbooks.sh
+++ getbooks.sh
@@ -1,13 +1,32 @@
-#!/bin/bash/
+#!/bin/bash
+# getbooks.sh
 
 # Input Quality Control
 
-# one file --> chk one argument
-# file contains a list of web addresses, plain text UTF-8 format --> chk format
 # address on Gutenberg.org --> check can wget
 
+# quality control: one argument
+if ! [[ $# -eq 1 ]]
+then
+    1>&2 printf 'invalid number of arguments; expected 1, got %d\n' $#
+    exit
+fi
+
+# quality control: UTF-8 format
+if ! $(grep -Eoq 'https://www.gutenberg.org/ebooks/[[:digit:]]+\.txt\.utf-8' $1)
+then
+    1>&2 printf 'invalid file format; expected at least one web address in plain text UTF-8 format'
+    exit
+fi
 
 # Effects
+
+# perform downloads
+
+for web in $(cat $1)
+do
+    
+done
 
 # scrub for author, title
 # chk book exists in repo
