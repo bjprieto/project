c426488975fa65b722a5893e801d33356edfe1f5
Benjamin Prieto <bjprieto@ucdavis.edu>
2023-06-02 20:36:48 -0700

    finished first draft of getbooks.sh

--- getbooks.sh
+++ getbooks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # getbooks.sh
 
+
 # Input Quality Control
 
 # address on Gutenberg.org --> check can wget
@@ -12,6 +13,13 @@ then
     exit
 fi
 
+# quality control: web address file exists
+if ! [[ -f $1 ]]
+then
+    1>&2 printf "invalid path; $1 is not a existing file\n"
+    exit
+fi
+
 # quality control: UTF-8 format
 if ! $(grep -Eoq 'https://www.gutenberg.org/ebooks/[[:digit:]]+\.txt\.utf-8' $1)
 then
@@ -20,7 +28,7 @@ then
 fi
 
 
-# Primary Function
+# Primary Function: Download and Sort Books
 
 # perform downloads
 for web in $(cat $1)
@@ -49,6 +57,7 @@ do
 	    # create corresponding directory and file
 	    mkdir $author
 	    mv $file "$author/$title.txt"
+	    
 	else
 
 	    # check book exists in the preexisting directory
@@ -56,12 +65,39 @@ do
 	    then
        		mv $file "$author/$title.txt"
 	    else
-		printf 'exists already\n'
+		rm $file
+		printf "$file already exists as $author/$title\n"
 	    fi
 	    
 	fi
-	
+
+    # skip download if default book file already exists; undergo same process
     else
-	printf 'exists already\n'
+	title=$(head -1 $file | sed 's/.\+[Ee]Book[[:space:]]of[[:space:]]//; s/,.\+//; s/[[:punct:]]//g; s/\ /_/g' | tr [:upper:] [:lower:])
+	author=$(head -1 $file | sed 's/.\+,[[:space:]]by[[:space:]]//; s/[[:space:]]$//;s/[[:punct:]]//g; s/\ /_/g' | tr [:upper:] [:lower:])
+	
+	# check block for info extraction
+	# echo $title
+	# echo $author
+
+	# check author directory exists
+	if ! [[ -d $author ]]
+	then
+
+	    # create corresponding directory and file
+	    mkdir $author
+	    mv $file "$author/$title.txt"
+	else
+
+	    # check book exists in the preexisting directory
+	    if ! [[ -f "$author/$title.txt" ]]
+	    then
+       		mv $file "$author/$title.txt"
+	    else
+		rm $file
+		printf "book already exists\n"
+	    fi
+	    
+	fi
     fi
 done
